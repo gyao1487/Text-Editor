1d87bff43566623ada3ff38e282bb3629a1a5928
Grace <112779767+grachimus@users.noreply.github.com>
2023-02-18 16:53:26 -0600

    created PUT method for database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,24 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Put method that accepts some content and adds it to the database
+export const putDb = async (content) =>{
+  try {
+    const jateDB = await openDB('jate','1');
+    const tx = jateDB.transaction('jate','readwrite') ;
+    const store = tx.objectStore('jate');
+    const request = store.put({id: 1, value: content});
+
+    const result = await request;
+    console.log('Data successfully saved to the database!', result);
+  } catch (err) {
+    console.error('Error opening jate database',err)
+  }
+};
+
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
+ 
 
 initdb();
