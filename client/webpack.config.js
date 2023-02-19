bb9960e2c83aedda680aa187e4c9eb7a59ef628c
Grace <112779767+grachimus@users.noreply.github.com>
2023-02-18 21:47:01 -0600

    completed webpack.config.js

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -18,7 +18,33 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin ({
+        template: './index.html'
+      }),
+
+      new WebpackPwaManifest({
+        name: 'JATE',
+        short_name:"JATE",
+        description: "Just Another Text Editor, PWA text-editor application",
+        display: 'standalone',
+        background_color: '#6C5B7B',
+        theme_color: '#C06C84',
+        start_url: "/",
+        public_path:"/",
+        inject: true,
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: "512x512",
+            destination: path.join('assets','icons')
+          }
+        ]
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',        
+      })
     ],
 
     module: {
