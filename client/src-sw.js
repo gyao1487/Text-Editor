05e8cd1f9f131242a37305aeb8ecd5cbcad8b79b
Grace <112779767+grachimus@users.noreply.github.com>
2023-02-18 21:25:21 -0600

    added comment

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,18 +1,18 @@
-const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
-const { registerRoute } = require('workbox-routing');
-const { CacheableResponsePlugin } = require('workbox-cacheable-response');
-const { ExpirationPlugin } = require('workbox-expiration');
-const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { offlineFallback, warmStrategyCache } = require("workbox-recipes");
+const { CacheFirst } = require("workbox-strategies");
+const { registerRoute } = require("workbox-routing");
+const { CacheableResponsePlugin } = require("workbox-cacheable-response");
+const { ExpirationPlugin } = require("workbox-expiration");
+const { precacheAndRoute } = require("workbox-precaching/precacheAndRoute");
 
 //precache all assets listend in WB MANIFEST file and configures cache-first strategy for them
 precacheAndRoute(self.__WB_MANIFEST);
 
 //Create "CacheFirst" instance named "page-cache"
 const pageCache = new CacheFirst({
-  cacheName: 'page-cache',
+  cacheName: "page-cache",
   plugins: [
-    //Cache responsees with status codes 0 and 200
+    //Cache responsees with status codes 0 and 200 (successful responses)
     new CacheableResponsePlugin({
       statuses: [0, 200],
     }),
@@ -25,28 +25,22 @@ const pageCache = new CacheFirst({
 
 //"warms up" the 'pageCache' strategy by pre-caching the following urls using CacheFirst
 warmStrategyCache({
-  urls: ['/index.html', '/'],
+  urls: ["/index.html", "/"],
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+registerRoute(({ request }) => request.mode === "navigate", pageCache);
 
-// Asset caching
+// Asset caching (static assets eg. stylesheets, scripts, web workers)
 registerRoute(
-  ({request}) => ['style','script','worker'].includes(request.destination),
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
+  //StaleWhileRevalidate - tells service worker to serve the cached asset while it revalidates the asset in the background
   new StaleWhileRevalidate({
-    cacheName: 'asset-cache',
+    cacheName: "asset-cache",
     plugins: [
       new CacheableResponsePlugin({
-        statuses: [0,200],
-      })
-    ]
+        statuses: [0, 200],
+      }),
+    ],
   })
 );
-
-//Fallback to offline page for navigation requests when there is no network
-registerRoute(
-  ({request}) => request.mode ==='navigate'
-  
-  })
-)
